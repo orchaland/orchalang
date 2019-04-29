bd6cba8bc32bed3564faaae223e931ef4afab98d
orchaland <orchaland@gmail.com>
2019-04-29 14:38:05 +0200

    delete me

--- orchalanguage/src/main/java/me/MeConfig.java
+++
@@ -1,8 +0,0 @@
-package me;
-
-import java.util.Properties;
-
-public class MeConfig extends Properties{
-
-    private static final long serialVersionUID = 1L;
-}
--- orchalanguage/src/main/java/me/MeConfigKey.java
+++
@@ -1,9 +0,0 @@
-package me;
-
-public class MeConfigKey {
-
-    public static final String NAME = "info.name";
-    public static final String AGE = "info.age";
-    public static final String GENDER = "info.gender";
-    public static final String ADDRESS = "info.address";
-}
